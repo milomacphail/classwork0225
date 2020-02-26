5fb19cb98f8e0efdf8b16717e46b97cfa08538b2
milomacphail <milomacphail@gmail.com>
2020-02-26 17:38:35 -0500

    PUT and DELETE frontend routes

--- public/script.js
+++ public/script.js
@@ -10,12 +10,12 @@ xhrGetAll.onload = () => {
     console.log(xhrGetAll);
     grads = JSON.parse(xhrGetAll.response);
 
-    if(xhrGetAll.readyState == 4 && xhrGetAll.status == 200){
+    if (xhrGetAll.readyState == 4 && xhrGetAll.status == 200) {
         console.log(grads);
     } else {
         console.error("Error!");
     }
-} 
+}
 
 const renderGrads = () => {
     console.log("click");
@@ -30,13 +30,14 @@ const renderGrads = () => {
             " , " +
             "Date of Graduation:" + element.yearOfGraduation +
             "</li>"
-        )})
-           document.getElementById("results").innerHTML = 
-           "<ul>" + gradList.join('\n') + "</ul>"; 
+        )
+    })
+    document.getElementById("results").innerHTML =
+        "<ul>" + gradList.join('\n') + "</ul>";
 }
 
 
-function addGraduate(e){
+function addGraduate(e) {
     e.preventDefault();
     console.log("Submit");
 
@@ -53,4 +54,45 @@ function addGraduate(e){
     xhrPost.setRequestHeader("Content-Type", "application/json");
     xhrPost.send(JSON.stringify(grad));
 
+}
+
+function editGraduate(e) {
+    e.preventDefault();
+    console.log("Editing open");
+
+    let grad = {};
+    grad.name = "Arnell";
+    grad.role = "Founder";
+    grad.company = "CareerDevs";
+    grad.yearOfGraduation = Date.now();
+
+    let xhrEdit = new XMLHttpRequest();
+    xhrEdit.open("PUT", url + "/id");
+    xhrEdit.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhrEdit.onload = () => {
+        let grads = JSON.parse(xhrEdit.response);
+        if (xhrEdit.readyState == 4 && xhrEdit.status == "200") {
+            console.log(grads);
+        } else {
+            console.error(grads);
+        }
+    }
+    xhrEdit.send(JSON.stringify(grad));
+}
+
+function deleteGrad(e) {
+    e.preventDefault();
+    console.log("Delete request");
+
+    let xhrDelete = new XMLHttpRequest();
+    xhrDelete.open("DELETE", url +"/id", true);
+    xhrDelete.onload = function () {
+        let grads = JSON.parse(xhrDelete.responseText);
+        if (xhrDelete.readyState == 4 && xhr.status == "200") {
+            console.table(grads);
+        } else {
+            console.error(grads);
+        }
+    }
+    xhrDelete.send(null);
 }
